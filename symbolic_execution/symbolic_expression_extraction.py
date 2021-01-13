c09d710ef4fce523d6e6d648c45372c4c464afd0
Wei-Cheng Wu <wwu@isi.edu>
2021-01-12 17:07:51 -0800

    Add symex to prefix feature

--- symbolic_execution/symbolic_expression_extraction.py
+++ symbolic_execution/symbolic_expression_extraction.py
@@ -2,6 +2,7 @@ import angr
 import claripy
 import re
 from collections.abc import Iterable
+from collections import deque
 
 
 from code_generation.c_code_generation import GeneratedCCode
@@ -115,4 +116,89 @@ class ExtractedSymExpr:
             else:
                 seq.append(token)
         return seq
+
+    def _try_merge_same_variable_concat(self, args):
+        # Try to match the form
+        # (a[7:0] .. a[15:8] .. a[23:16] .. a[31:24])
+
+        # Check if all args are 'BVV'
+        for arg in args:
+            if not isinstance(arg, claripy.ast.bv.BV) \
+               or str(arg.op) != "Extract":
+                return None
+
+        # Check if they are extracting from the same variable
+        var = args[0].args[2].args[0]
+        for arg in args:
+            if arg.args[2].args[0] != var:
+                return None
+
+        # Check if the extracting bit indexes are in sequence
+        arg = args.pop(0)
+        idx = arg.args[0]
+        start = arg.args[1]
+        while len(args) > 0:
+            arg = args.pop(0)
+            if arg.args[1] != idx + 1:
+                return None
+            idx = arg.args[0]
+        # Discard "Extract" expression
+        #return ["Extract", "(%s, %s)" % (idx, start), var]
+        return [var]
+
+
+    def _symex_to_prefix(self, expr):
+        op = str(expr.op)
+
+        if op == "BVV":
+            # Constant value
+            return [str(hex(expr.args[0]))]
+
+        elif op == "Concat":
+            # Handle special case of
+            # (a[7:0] .. a[15:8] .. a[23:16] .. a[31:24])
+            # If not this case, do nothing here
+            queue = self._try_merge_same_variable_concat(list(expr.args))
+            if queue is not None:
+                return queue
+
+        elif op == "Extract":
+            # Variable Extraction
+            # Discard "Extract" expression
+            #return [op, str((expr.args[0], expr.args[1])), expr.args[2].args[0]]
+            return [expr.args[2].args[0]]
+
+        elif op == "BVS":
+            # FIXME:
+            # if we extracted variable at "Extract", won't execute here
+            return [str(expr.args[0])]
+
+        children = []
+        ast_queue = deque([iter(expr.args)])
+        while ast_queue:
+            try: 
+                ast = next(ast_queue[-1])
+            except StopIteration:
+                ast_queue.pop()
+                continue
+            children.append(self._symex_to_prefix(ast))
+
+        # Claripy will merge same operations into one ast
+        # e.g. (a + b + c) will be one ast, "+" with 3 args
+        if len(expr.args) == 2:
+            return [op] + children[0] + children[1]
+        else:
+            prefix_queue = []
+            while len(children) > 1:
+                prefix_queue += [op]
+                prefix_queue += children.pop(0)
+            prefix_queue += children.pop(0)
+            return prefix_queue
+
+
+
+    def symex_to_prefix(self):
+        symex_expr = self.symex_expr
+        return self._symex_to_prefix(symex_expr)
+
 
