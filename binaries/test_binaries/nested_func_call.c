43a3c1dfe65800dd6ada562b996cbe6bbf29c5f8
Nicolaas <nweidema@usc.edu>
2021-04-14 10:15:02 -0700

    Update nested_func_call source and binary to allow for more extensive function short circuiting tests.

--- binaries/test_binaries/nested_func_call.c
+++ binaries/test_binaries/nested_func_call.c
@@ -2,13 +2,43 @@
 #include <stdlib.h>
 #include <math.h>
 
-float f_inner(float y) 
+float f_inner(float y)
 {
     // Something to blow up symbolic execution
     while (1) {
         y++;
     }
     return y;
+}
+
+int f_inner2(int x, int y)
+{
+    // Something to blow up symbolic execution
+    while (1) {
+        x++;
+        y++;
+    }
+    return x + y;
+}
+
+int f_inner3(int x, float y)
+{
+    // Something to blow up symbolic execution
+    while (1) {
+        x++;
+        y++;
+    }
+    return x + (int)y;
+}
+
+float f_inner4(float x, int y)
+{
+    // Something to blow up symbolic execution
+    while (1) {
+        x++;
+        y++;
+    }
+    return x + (float)y;
 }
 
 
@@ -24,6 +54,21 @@ float f02(float x, float y)
     return sin(x) + f_inner(y);
 }
 
+int f03(int x, int y)
+{
+    return f_inner2(x, y);
+}
+
+int f04(int x, float y)
+{
+    return f_inner3(x, y);
+}
+
+float f05(float x, int y)
+{
+    return f_inner4(x, y);
+}
+
 int main(int argc, char *argv[])
 {
     float x, y, a;
@@ -37,6 +82,9 @@ int main(int argc, char *argv[])
     printf("a: %f\n", a);
 
     a = f02(x, y);
+    printf("a: %f\n", a);
+
+    a = f03((int)x, (int)y);
     printf("a: %f\n", a);
     return EXIT_SUCCESS;
 }
