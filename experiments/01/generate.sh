f967b39626e021b589a563f4583a207f5d197577
Katy Felkner <felkner@isi.edu>
2022-07-21 17:56:17 -0700

    remove old, redundant files

--- experiments/01/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-fairseq-generate data-bin/ \
-    --path checkpoints/checkpoint_best.pt \
-    --beam 5 --remove-bpe
--- experiments/01/preprocess.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-fairseq-preprocess --source-lang sym --target-lang math \
-	--trainpref train/train --validpref train/valid --testpref train/test \
-	--destdir data-bin --workers 20
-
